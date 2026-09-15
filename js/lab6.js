3ef68ffd956107a4c6da6d16aae767045df35634
HarryC919 <HarryC919@users.noreply.github.com>
2026-09-15 11:53:42 +0000

    deploy: HarryC919/DKU-STATS-401@326e5e38a5ad0fa37e59cdb5e5117c5ffaa2ca73

---
+++ js/lab6.js
@@ -0,0 +1,7 @@
+async function drawChart() {
+    // ToDO: Implement the drawChart function for lab6
+}
+;
+drawChart();
+export {};
+//# sourceMappingURL=lab6.js.map
---
+++ src/lab6.ts
@@ -0,0 +1,7 @@
+export {};
+
+async function drawChart(): Promise<void> {
+    // ToDO: Implement the drawChart function for lab6
+};
+
+drawChart();
